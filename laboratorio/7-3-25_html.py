54072e72dd138842a04501560ea79cd87e3241d0
ripamonti-daniele <ripamonti.daniele.08@itisdalmine.edu.it>
2025-03-13 21:10:48 +0100

    consegna compito lab 7-3-25

--- laboratorio/7-3-25_html.py
+++ laboratorio/7-3-25_html.py
@@ -1,26 +1,20 @@
-f = open("Ripamonti_Daniele_3Ai_2425\\laboratorio\\template.txt", "r", encoding="utf-8")
-testo = f.read()
-
-# parole = testo.split()
+f = open("laboratorio\\template.txt", "r", encoding="utf-8")
 
 riscrivi = {"%titoloPagina%": "doc1", "%colore%": "FF0000", "%nome%": "Daniele", "%cognome%": "Ripamonti"}
-conto_perc = 0
-for j, i in enumerate(testo):
-    t = ""
-    pos = 0
-    if i == "%" and (conto_perc % 2 == 0 or conto_perc == 0):
-        t += testo[pos:j]
-        pos = j
-        conto_perc += 1
-        # print("a", conto_perc, pos)
-           
-    if i == "%" and conto_perc != 0 and conto_perc % 2 != 0:
-        print(testo[pos:j + 1], j + 1, pos)
-        conto_perc += 1
-        # print("ok", conto_perc)
-        if testo[pos:j + 1] in riscrivi:
-            t += riscrivi[testo[pos:j + 1]]
-            print("ok")  
-        pos = j + 1
 
-# print(t)
+nuovo = ""
+
+for i in f:
+    for key in riscrivi:
+        if key in i:
+            i = i.replace(key, riscrivi[key])
+            
+    nuovo += i
+
+f.close()
+
+f = open("laboratorio\\template.txt", "w", encoding="utf-8")
+
+f.write(nuovo)
+
+f.close()
